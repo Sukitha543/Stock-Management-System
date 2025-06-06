8916c2d73f07aecd9b9f953f3341322ad3357c42
Sukitha Chathuranga Hettimudalige <sukithachathuranga@gmail.com>
2025-06-07 00:19:11 +0530

    Add Employee class

---
+++ Stock Management System/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Management_System
+{
+    internal class Employee
+    {
+        private string id;
+        private string firstName;
+        private string lastName;
+        private int contactNo;
+        private string email;
+        private string address;
+
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string FirstName
+        {
+            get {return firstName;}
+            set { firstName = value;}
+        }
+
+        public string LastName
+        {
+            get {return lastName;}
+            set { lastName = value;}
+        }
+
+        public int ContactNo
+        {
+            get {return contactNo;}
+            set { contactNo = value;}
+        }
+
+        public string Email
+        {
+            get {return email;}
+            set { email = value;}
+        }
+
+        public string Address
+        {
+            get {return address;}
+            set { address = value;}
+        }
+    }
+}
