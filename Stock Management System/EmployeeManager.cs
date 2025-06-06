9fcf45bfbf0af997fee3fd55a5383f2a2990aac7
Sukitha Chathuranga Hettimudalige <sukithachathuranga@gmail.com>
2025-06-07 00:30:47 +0530

    Add EmployeeManager Class

---
+++ Stock Management System/EmployeeManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Management_System
+{
+
+    internal class EmployeeManager : Person, Interface
+    {
+        
+
+        public EmployeeManager(string username, string id, string password) : base(username, id, password)
+        {
+        }
+
+        public EmployeeManager(string username, string password) : base(username, password)
+        {
+        }
+
+        public override void create(string username, string id, string password)
+        {
+            
+        }
+
+        public override void login(string username, string password)
+        {
+            
+        }
+    }
+}
