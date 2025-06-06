5de035460b7ee345382570b0c14824bbd4b50b11
Sukitha Chathuranga Hettimudalige <sukithachathuranga@gmail.com>
2025-06-07 00:08:14 +0530

    Add AddEmployees UI

--- Stock Management System/AddEmployees.cs
+++ Stock Management System/AddEmployees.cs
@@ -16,5 +16,10 @@ namespace Stock_Management_System
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
